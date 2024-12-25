8dc7c6fdfb4e68960f10d6fe46ac651a9be9a032
Краснова Светлана <svetlana.krasnova@inside.glowbyte.com>
2024-12-25 16:15:11 +0300

    using changed

--- example.py
+++ example.py
@@ -1,24 +1,17 @@
 import cv2
 import numpy as np
 import map_code
-from skimage.metrics import structural_similarity as ssim
-from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-def objective_function(params, image, illumination_map):
-    a, b = params
-    corrected_image = (image / (a * illumination_map + b))
-    ssim_value = -ssim(image, corrected_image, data_range=image.max() - image.min(), win_size=7, channel_axis=-1)
-    regularization = (a - 1) ** 2 
-    return ssim_value + regularization
-
 image = cv2.imread("39.jpg")
-il_map = map_code.get_map(image)
-mask_3ch = np.repeat(il_map[:, :, np.newaxis], 3, axis=2)
-initial_params = [1, 0.5]
-bounds = [(0.1, 2), (0, 1)]
-result = minimize(objective_function, initial_params, args=(image, mask_3ch), bounds=bounds)
-a_opt, b_opt = result.x
-corrected_image = image / (a_opt * mask_3ch + b_opt)
-res = cv2.normalize(corrected_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
-cv2.imwrite("39_out.jpg", res)
+display = cv2.imread("L1.jpg")
+il_map = map_code.get_map(display)
+
+max_intens = np.max(il_map)
+delta = 1 - max_intens
+il_map1 = il_map + delta
+
+mask_3ch = np.repeat(il_map1[:, :, np.newaxis], 3, axis=2)
+corrected_image = image / mask_3ch
+#res = cv2.normalize(corrected_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
+cv2.imwrite("39_out_1.jpg", np.uint8(corrected_image))
--- example_mirror.py
+++ example_mirror.py
@@ -1,26 +1,17 @@
 import cv2
 import numpy as np
-from skimage.metrics import structural_similarity as ssim
-from scipy.optimize import minimize
-
-def objective_function(params, image, illumination_map):
-    a, b = params
-    corrected_image = (image / (a * illumination_map + b))
-    ssim_value = -ssim(image, corrected_image, data_range=image.max() - image.min(), win_size=7, channel_axis=-1)
-    regularization = (a - 1) ** 2 
-    return ssim_value + regularization
-
+import matplotlib.pyplot as plt
 
 image = cv2.imread("39.jpg")
 
-mirror = cv2.imread('mirror3200.jpg')
+mirror = cv2.imread('mirror1200.jpg')
 mirror = cv2.cvtColor(mirror, cv2.COLOR_BGR2GRAY)
 mirror = mirror / 255
-mirror = mirror/np.max(mirror)
-mask_3ch = np.repeat(mirror[:, :, np.newaxis], 3, axis=2)
-initial_params = [1, 0.5]
-bounds = [(0.1, 2), (0.01, 1)]
-result = minimize(objective_function, initial_params, args=(image, mask_3ch), bounds=bounds)
-a_opt, b_opt = result.x
-corrected_im = image / (a_opt * mask_3ch + b_opt)
-cv2.imwrite(f"39_corr.jpg", corrected_im)
+mirror = cv2.GaussianBlur(mirror, (51, 51), 0)
+delta = 1 - np.max(mirror)
+
+mirror1 = mirror + delta
+
+mask_3ch = np.repeat(mirror1[:, :, np.newaxis], 3, axis=2)
+corrected_im = image / mask_3ch
+cv2.imwrite(f"39_mirror_51.jpg", np.uint8(corrected_im))
