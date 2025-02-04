d416a1330695955d6b3b4716599567608c0a2e52
Краснова Светлана <svetlana.krasnova@inside.glowbyte.com>
2025-02-05 02:48:16 +0300

    clip intensities added

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ import numpy as np
 import map_code
 import matplotlib.pyplot as plt
 
-image = cv2.imread("39.jpg")
+image = cv2.imread("06.jpg")
+image = image / 255
 display = cv2.imread("L14.jpg")
 il_map = map_code.get_map(display)
 
@@ -13,5 +14,7 @@ il_map1 = il_map + delta
 
 mask_3ch = np.repeat(il_map1[:, :, np.newaxis], 3, axis=2)
 corrected_image = image / mask_3ch
+corrected_image = np.clip(corrected_image, 0, 1)
+
 #res = cv2.normalize(corrected_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
-cv2.imwrite("39_out.jpg", np.uint8(corrected_image))
+cv2.imwrite("06_out_gauss.jpg", np.uint8(corrected_image * 255))
--- example_mirror.py
+++ example_mirror.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("39.jpg")
-
+image = cv2.imread("04.jpg")
+image = image / 255
 mirror = cv2.imread('mirror1200.jpg')
 mirror = cv2.cvtColor(mirror, cv2.COLOR_BGR2GRAY)
 mirror = mirror / 255
@@ -14,4 +14,6 @@ mirror1 = mirror + delta
 
 mask_3ch = np.repeat(mirror1[:, :, np.newaxis], 3, axis=2)
 corrected_im = image / mask_3ch
-cv2.imwrite(f"39_out_mirror.jpg", np.uint8(corrected_im))
+corrected_im = np.clip(corrected_im, 0, 1)
+
+cv2.imwrite(f"04_out_mirror.jpg", np.uint8(corrected_im * 255))
